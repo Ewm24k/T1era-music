8b1d5bccb8b880a629835d0b9488d07855cab231
Boss Tam <151812107+Ewm24k@users.noreply.github.com>
2026-08-31 11:53:15 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { auth, googleProvider } from "./firebase-config.js";
 import {
-  getAuth,
   signInWithEmailAndPassword,
-  GoogleAuthProvider,
   signInWithPopup,
   onAuthStateChanged,
   signOut,
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-
-// Firebase App Configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDpVqwUVpM2c41y2RF5IlPQwKW71iyyhc8",
-  authDomain: "t1era-musicv1.firebaseapp.com",
-  projectId: "t1era-musicv1",
-  storageBucket: "t1era-musicv1.firebasestorage.app",
-  messagingSenderId: "878684058813",
-  appId: "1:878684058813:web:58f21cf930740fa68bb3d4",
-  measurementId: "G-XX6MGLBDE4",
-};
-
-// Initialize Firebase App
-let app, auth, googleProvider;
-try {
-  app = initializeApp(firebaseConfig);
-  auth = getAuth(app);
-  googleProvider = new GoogleAuthProvider();
-} catch (e) {
-  console.error("Firebase SDK initialization error:", e);
-}
 
 // UI DOM Elements
 const overlay = document.getElementById("interactive-overlay");
