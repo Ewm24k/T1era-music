9666e9a2154f9daf51dd8a9ae01234c3665c80c5
Boss Tam <151812107+Ewm24k@users.noreply.github.com>
2026-09-02 14:25:15 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
 // Pautan URL Pelayan GCP VM API T1era Music (Menggunakan Terowong Selamat Ngrok)
-const RENDER_BACKEND_URL = "https://d040-35-247-154-247.ngrok-free.app/transcribe";
+const RENDER_BACKEND_URL = "https://9fd5-35-247-154-247.ngrok-free.app/transcribe";
 
 // =========================================================
 // INJEKSI STYLING NEON KONSOL PEMPROSESAN (AAA GRADE UI/UX)
